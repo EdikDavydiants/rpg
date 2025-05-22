acac20689311efcb11a7b6fbfab3cc20cdcc4028
Edik <davydiantsedik@mail.ru>
2025-05-22 21:49:23 +0300

    ui-форма соединения с сервером пропадает при успешном подключении

--- src/main/java/ed/av/rpg/form/connection/ConnectionManager.java
+++ src/main/java/ed/av/rpg/form/connection/ConnectionManager.java
@@ -1,13 +1,17 @@
 package ed.av.rpg.form.connection;
 
+import ed.av.rpg.Logger;
 import ed.av.rpg.auth.connection.ConnectionData;
 import ed.av.rpg.config.MainStompSessionHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
 public class ConnectionManager {
@@ -25,6 +29,13 @@ public class ConnectionManager {
         WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
-        stompClient.connectAsync(serverUrl, handler);
+        CompletableFuture<StompSession> futureSession = stompClient.connectAsync(serverUrl, handler);
+
+        futureSession.thenAccept(session -> {
+            form.getNode().setVisible(false);
+        }).exceptionally(ex -> {
+            Logger.log("Не удалось соединиться с сервером!");
+            return null;
+        });
     }
 }
